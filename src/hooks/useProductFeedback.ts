b97a07cf74cbbf2516108d37de0891bd5a659b86
Vinci Taylaran <37849496+vincitaylaran@users.noreply.github.com>
2021-09-25 12:48:59 -0600

    Resolve #6
    - Updated the Comment and ProductRequest interfaces
    - Created a ProductFeedback interface

--- src/hooks/useProductFeedback.ts
+++ src/hooks/useProductFeedback.ts
@@ -1,18 +1,25 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import data from '../data.json';
+
+interface ProductFeedback {
+  currentUser: User;
+  productRequests: ProductRequest[];
+}
 
 interface ProductRequest {
   id: number;
   title: string;
-  category: 'feature' | 'bug' | 'enhancement';
+  category: string;
   upvotes: number;
   description: string;
-  comments: Comment[];
+  comments?: Comment[];
 }
 
 interface Comment {
   id: number;
   content: string;
   user: User;
+  replyingTo?: string;
 }
 
 interface User {
@@ -21,6 +28,18 @@ interface User {
   username: string;
 }
 
-function useProductRequests() {
-  const [feedback, setFeedback] = useState<ProductRequest[]>([]);
+export function useProductFeedback() {
+  const [feedback, setFeedback] = useState<ProductFeedback>();
+
+  useEffect(() => {
+    const fetchData = (): void => {
+      setFeedback(data);
+    };
+
+    fetchData();
+  }, [feedback]);
+
+  return {
+    feedback,
+  };
 }
