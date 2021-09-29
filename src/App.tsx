8a92c5e87838a23e33a7bcd36e2f361c8a38c14f
Justin Morgan <justin.morgan@hey.com>
2021-09-29 10:17:21 -0700

    :construction:  Working on components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useProductFeedback } from './hooks/useProductFeedback';
 import data from './data.json';
+import Rainbox from './components/Rainbox';
+import PageContainer from './components/PageContainer';
+import FilterBox from './components/FilterBox';
+import Roadmap from './components/Roadmap';
+import Stack from './components/Stack';
 
 interface AppProps {}
 
@@ -8,9 +13,16 @@ function App({}: AppProps) {
   const { feedback, upvoteProductRequest } = useProductFeedback(data);
 
   return (
-    <div>
-      <h1>Product feedback!!!</h1>
-    </div>
+    <PageContainer>
+      <Stack>
+        <Rainbox>
+          <h1>Frontend Mentor</h1>
+          <h2>Feedback Board</h2>
+        </Rainbox>
+        <FilterBox />
+        <Roadmap />
+      </Stack>
+    </PageContainer>
   );
 }
 
