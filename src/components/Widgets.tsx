5a356d6ddf33acaaacfcaf4e36efef7b742d5b4f
Vinci Taylaran <37849496+vincitaylaran@users.noreply.github.com>
2021-11-04 13:02:23 -0600

    Create container for widgets content, create new props for Widgets

--- src/components/Widgets.tsx
+++ src/components/Widgets.tsx
@@ -1,12 +1,39 @@
 import React from 'react';
 import styles from '../scss/Widgets.module.scss';
+import ShadowBackground from './ShadowBackground';
+import FilterBox, { FilterBoxProps } from './FilterBox';
+import Roadmap, { RoadmapProps } from './Roadmap';
 
-interface WidgetsProps {
+interface WidgetsProps extends FilterBoxProps, RoadmapProps {
+  visible: boolean;
+}
+
+function Widgets({
+  currentFilter,
+  filterByCategory,
+  productRequests,
+  visible,
+}: WidgetsProps): JSX.Element {
+  return (
+    <div className={`${styles.widgets} ${visible ? '' : styles.hidden}`}>
+      <ShadowBackground />
+      <WidgetsContent>
+        <FilterBox
+          currentFilter={currentFilter}
+          filterByCategory={filterByCategory}
+        />
+        <Roadmap productRequests={productRequests} />
+      </WidgetsContent>
+    </div>
+  );
+}
+
+interface WidgetsContentProps {
   children: React.ReactNode;
 }
 
-function Widgets({ children }: WidgetsProps): JSX.Element {
-  return <div className={styles.widgets}>{children}</div>;
+function WidgetsContent({ children }: WidgetsContentProps): JSX.Element {
+  return <div className={styles.content}>{children}</div>;
 }
 
 export default Widgets;
