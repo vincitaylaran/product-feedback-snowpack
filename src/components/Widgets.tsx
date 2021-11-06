b65f5380f11c3f9302c00725d6df58c62943b220
Vinci Taylaran <37849496+vincitaylaran@users.noreply.github.com>
2021-11-05 18:42:20 -0600

    Add animation to widgets mobile to slide from left

--- src/components/Widgets.tsx
+++ src/components/Widgets.tsx
@@ -3,6 +3,7 @@ import styles from '../scss/Widgets.module.scss';
 import ShadowBackground from './ShadowBackground';
 import FilterBox, { FilterBoxProps } from './FilterBox';
 import Roadmap, { RoadmapProps } from './Roadmap';
+import { animated, useSpring } from 'react-spring';
 
 interface WidgetsProps extends FilterBoxProps, RoadmapProps {
   visible: boolean;
@@ -17,23 +18,41 @@ function Widgets({
   return (
     <div className={`${styles.widgets}`}>
       <ShadowBackground visible={visible} />
-      {/* <WidgetsContent>
+      <WidgetsContent visible={visible}>
         <FilterBox
           currentFilter={currentFilter}
           filterByCategory={filterByCategory}
         />
         <Roadmap productRequests={productRequests} />
-      </WidgetsContent> */}
+      </WidgetsContent>
     </div>
   );
 }
 
 interface WidgetsContentProps {
   children: React.ReactNode;
+  visible?: boolean;
 }
 
-function WidgetsContent({ children }: WidgetsContentProps): JSX.Element {
-  return <div className={styles.content}>{children}</div>;
+function WidgetsContent({
+  children,
+  visible,
+}: WidgetsContentProps): JSX.Element {
+  const props = useSpring({
+    width: window.innerWidth * 0.75,
+    height: '100%',
+    padding: '24px',
+    gridGap: '24px',
+    display: visible ? 'grid' : 'none',
+    gridAutoRows: 'min-content',
+    position: 'absolute',
+    left: visible ? window.innerWidth * -0.75 : 0,
+    background: 'black',
+  });
+
+  console.log('inner width', window.innerWidth);
+
+  return <animated.div style={props}>{children}</animated.div>;
 }
 
 export default Widgets;
