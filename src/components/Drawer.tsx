30360d8d937cf36640e28e8315ed71e26e09ab39
Vinci Taylaran <37849496+vincitaylaran@users.noreply.github.com>
2021-11-08 13:17:24 -0700

    Refactor Widgets and rename to Drawer

---
+++ src/components/Drawer.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import styles from '../scss/Drawer.module.scss';
+import ShadowBackground from './ShadowBackground';
+import FilterBox, { FilterBoxProps } from './FilterBox';
+import Roadmap, { RoadmapProps } from './Roadmap';
+import { motion } from 'framer-motion';
+
+const isViewingFromMobileDevice = window.innerWidth <= 480;
+
+// interface DrawerProps extends FilterBoxProps, RoadmapProps {
+//   visible: boolean;
+// }
+
+interface DrawerProps {
+  children: React.ReactNode;
+  isOpen: boolean;
+}
+
+function Drawer({ children, isOpen }: DrawerProps): JSX.Element {
+  return (
+    <motion.div
+      className={styles.drawer}
+      animate={isOpen ? 'open' : 'closed'}
+      variants={variants}
+      transition={{
+        default: { duration: 0.25 },
+      }}
+    >
+      {children}
+    </motion.div>
+  );
+}
+
+const variants = {
+  open: { x: 0 },
+  closed: { x: '100%' },
+};
+
+export default Drawer;
