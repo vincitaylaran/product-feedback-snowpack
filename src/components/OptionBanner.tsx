afb7cf5166c7419982d1b7cea4ed6f72a380a8de
Vinci Taylaran <37849496+vincitaylaran@users.noreply.github.com>
2021-11-11 12:49:34 -0700

    Redo styles for OptionBanner

--- src/components/OptionBanner.tsx
+++ src/components/OptionBanner.tsx
@@ -2,26 +2,24 @@ import React from 'react';
 import Card from './Card';
 import PillButton from './PillButton';
 
-import SuggestionIcon from '../../../public/assets/suggestions/icon-suggestions.svg';
-import PlusIcon from '../../../public/assets/shared/icon-plus.svg';
+import SuggestionIcon from '../../public/assets/suggestions/icon-suggestions.svg';
 import styles from '../scss/OptionBanner.module.scss';
-
-enum SortOrder {
-  MostUpvotes = 'Most Upvotes',
-  LeastUpvotes = 'Least Upvotes',
-  Status = 'Current Status',
-}
+import _ from 'lodash';
 
 interface OptionBannerProps {
   suggestionLength: number;
-  initialSortOrder?: SortOrder;
 }
+
 function OptionBanner({ suggestionLength }: OptionBannerProps) {
   return (
-    <Card className={styles.optionbanner}>
-      <img src={SuggestionIcon} />
-      <h3 className={styles.suggestion_count}>
-        {suggestionLength > 0 ? suggestionLength : 0} Suggestion
+    <Card className={styles.optionBanner}>
+      <img
+        className={styles.suggestionIcon}
+        src={SuggestionIcon}
+        alt="Suggestion icon"
+      />
+      <h3 className={styles.suggestionCount}>
+        {suggestionLength} Suggestion
         {suggestionLength == 1 ? '' : 's'}
       </h3>
       <OptionsDropdown />
@@ -31,28 +29,41 @@ function OptionBanner({ suggestionLength }: OptionBannerProps) {
 }
 
 interface OptionsDropdownProps {}
+
+type DropdownOptions =
+  | 'most-upvotes'
+  | 'least-upvotes'
+  | 'most-comments'
+  | 'least-comments';
+
 function OptionsDropdown({}: OptionsDropdownProps) {
+  const options: DropdownOptions[] = [
+    'most-upvotes',
+    'least-upvotes',
+    'most-comments',
+    'least-comments',
+  ];
+
   return (
-    <div className={styles.dropdown__container}>
-      <span>Sort by :</span>
-      <select name="sort_options" className={styles.dropdown__select}>
-        {Object.entries(SortOrder).map(([key, value]) => (
-          <option key={key} value={key}>
-            {value}
-          </option>
-        ))}
+    <div className={styles.dropdownContainer}>
+      <label htmlFor="sort-options">Sort by : </label>
+      <select name="sort-options">
+        {options.map((option) => {
+          let optionCapitalized = _.replace(option, '-', ' ');
+
+          return <option value={option}>{optionCapitalized}</option>;
+        })}
       </select>
-      <div className={styles.dropdown__downarrow}></div>
     </div>
   );
 }
 
 interface AddFeedbackButtonProps {}
+
 function AddFeedbackButton({}: AddFeedbackButtonProps) {
   return (
     <button className={styles.addFeedbackButton}>
-      <img src={PlusIcon} />
-      Add Feedback
+      <span>+ Add Feedback</span>
     </button>
   );
 }
