d1c370df5a141f4e751e79fbb29a6d25f32a5dfc
Vinci Taylaran <37849496+vincitaylaran@users.noreply.github.com>
2021-11-11 12:50:32 -0700

    Make MobileNav accept only children

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -1,42 +1,12 @@
 import React from 'react';
 import styles from '../scss/MobileNav.module.scss';
-import Drawer, { DrawerProps } from './Drawer';
-import FilterBox, { FilterBoxProps } from './FilterBox';
-import HamburgerIcon, { HamburgerIconProps } from './HamburgerIcon';
-import Rainbox from './Rainbox';
-import Roadmap, { RoadmapProps } from './Roadmap';
 
-interface MobileNavProps extends FilterBoxProps, RoadmapProps {
-  isDrawerOpen: boolean;
-  onHamburgerClick: () => void;
+interface MobileNavProps {
+  children: React.ReactNode;
 }
 
-function MobileNav({
-  isDrawerOpen,
-  filterByCategory,
-  currentFilter,
-  productRequests,
-  onHamburgerClick,
-}: MobileNavProps) {
-  return (
-    <div className={styles.mobileNav}>
-      <Rainbox>
-        <div>
-          <h1>Frontend Mentor</h1>
-          <h2>Feedback Board</h2>
-        </div>
-        <HamburgerIcon onClick={onHamburgerClick} isOpen={isDrawerOpen} />
-
-        <Drawer isOpen={isDrawerOpen}>
-          <FilterBox
-            filterByCategory={filterByCategory}
-            currentFilter={currentFilter}
-          />
-          <Roadmap productRequests={productRequests} />
-        </Drawer>
-      </Rainbox>
-    </div>
-  );
+function MobileNav({ children }: MobileNavProps) {
+  return <div className={styles.mobileNav}>{children}</div>;
 }
 
 export default MobileNav;
