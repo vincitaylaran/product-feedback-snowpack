08f5aba11d5c972a2dc34de02382aebbcc686a51
Vinci Taylaran <37849496+vincitaylaran@users.noreply.github.com>
2021-11-27 12:59:36 -0700

    Create gql query to fetch one comment

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -1,11 +1,67 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { useQuery, gql } from '@apollo/client';
+import Loading from './Loading';
 
-// TODO: use Apollo Provider to fetch a request with the ID in params.
+const GET_COMMENT = gql`
+  query Requests($requestId: Int!) {
+    Request(id: $requestId) {
+      id
+      title
+      description
+      category
+      upvotes {
+        id
+      }
+      comments {
+        id
+        content
+        user {
+          id
+          name
+          username
+          image
+        }
+        replies {
+          id
+          content
+          user {
+            id
+            name
+            username
+            image
+          }
+          userId
+        }
+      }
+    }
+  }
+`;
+
 function Comment() {
   let params = useParams();
+  const { data, loading, error } = useQuery(GET_COMMENT, {
+    variables: { requestId: Number(params.requestId) },
+  });
+  const { title, description, category, upvotes, comments } = data.Request;
 
-  return <div>Comment: #{params.requestId}</div>;
+  if (error) console.error(error);
+  if (loading) return <Loading />;
+  if (data) console.log(data);
+
+  return (
+    <div>
+      <div>{title}</div>
+      <div>{description}</div>
+      <div>{category}</div>
+      <div>{upvotes.length}</div>
+      <div>
+        {comments.map((c: any) => (
+          <div>{c.content}</div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Comment;
