78d9a8b1221bce38f608d55571feffe1728f22e7
Vinci Taylaran <37849496+vincitaylaran@users.noreply.github.com>
2021-11-28 11:25:17 -0700

    Update comments.tsx

--- src/routes/comments.tsx
+++ src/routes/comments.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Link, Outlet } from 'react-router-dom';
+import Button from '../components/Button';
+import GoBackButton from '../components/GoBackButton';
+import PageLayout, { PageNav } from '../components/PageLayout';
 
 function Comments() {
   return (
-    <main>
-      <h1>Comments</h1>
+    <PageLayout>
+      <PageNav>
+        <GoBackButton />
+        <Button>Edit Feedback</Button>
+      </PageNav>
       <Outlet />
-    </main>
+    </PageLayout>
   );
 }
 
