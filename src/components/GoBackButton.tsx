0f21a5d439592505c9a0d7c3c9346ab9814b0552
Vinci Taylaran <37849496+vincitaylaran@users.noreply.github.com>
2021-11-30 13:37:06 -0700

    Make GoBackButton link to home page

--- src/components/GoBackButton.tsx
+++ src/components/GoBackButton.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import styles from '../scss/GoBackButton.module.scss';
 import LeftArrow from '../../public/assets/shared/icon-arrow-left.svg';
+import { Link } from 'react-router-dom';
 
 function GoBackButton() {
   return (
-    <div className={styles.goBackBtn}>
+    <Link to="/" className={styles.goBackBtn}>
       <img src={LeftArrow} alt="Left arrow" />
       <span>Go Back</span>
-    </div>
+    </Link>
   );
 }
 
