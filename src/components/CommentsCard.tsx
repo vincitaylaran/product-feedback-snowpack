0fa67f71013e2e821b60374a4c1c0755b9a3df21
Vinci Taylaran <37849496+vincitaylaran@users.noreply.github.com>
2021-12-01 14:10:32 -0700

    Update styles for CommentsCard

--- src/components/CommentsCard.tsx
+++ src/components/CommentsCard.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Card from './Card';
 import type { Comment } from '../interfaces/productRequest.interface';
+import type { User } from '../interfaces/user.interface';
 import styles from '../scss/CommentsCard.module.scss';
 
 interface CommentsCardProps {
@@ -8,25 +9,57 @@ interface CommentsCardProps {
 }
 
 function CommentsCard({ comments }: CommentsCardProps) {
+  // Temporary solution to fetching user's profile pic.
+  const getUserImg = (user: User) => {
+    const [firstName] = user.name.split(' ');
+    return `../assets/user-images/image-${firstName.toLowerCase()}.jpg`;
+  };
+
+  const getCommentCount = () => {
+    let commentsCount = comments.length;
+    let repliesCount = 0;
+
+    comments.forEach((comment) => {
+      if (comment.replies) {
+        repliesCount += comment.replies.length;
+      }
+    });
+
+    return commentsCount + repliesCount;
+  };
+
   return (
     <Card className={styles.commentsCard}>
-      <h3 className={styles.commentsCard__count}>{comments.length} Comments</h3>
+      <h3 className={styles.commentsCard__count}>
+        {getCommentCount()} Comments
+      </h3>
       <div className={styles.commentsCard__comments}>
         {comments.map((comment) => (
-          <div className={styles.comment}>
+          <div className={styles.comment} key={comment.id}>
             <div className={styles.main}>
               <div className={styles.heading}>
-                <img className={styles.profilePic} />
-                <div className={styles.userInfo}></div>
+                <img
+                  className={styles.profilePic}
+                  src={getUserImg(comment.user)}
+                  alt="Profile picture"
+                />
+                <div className={styles.userInfo}>
+                  <div className={styles.name}>{comment.user.name}</div>
+                  <div className={styles.username}>
+                    @{comment.user.username}
+                  </div>
+                </div>
                 <button className={styles.replyBtn}>Reply</button>
               </div>
-              <div className={styles.content}>{comment.content}</div>
+              <p className={styles.content}>{comment.content}</p>
             </div>
 
             {comment.replies && (
               <div className={styles.replies}>
                 {comment.replies.map((reply) => (
-                  <div className={styles.reply}>{reply.content}</div>
+                  <p className={styles.reply} key={reply.id}>
+                    {reply.content}
+                  </p>
                 ))}
               </div>
             )}
