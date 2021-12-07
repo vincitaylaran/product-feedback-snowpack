15fda07789cb9a03311bd8b1d81857261d30b751
Vinci Taylaran <37849496+vincitaylaran@users.noreply.github.com>
2021-12-07 13:09:57 -0700

    Update input text to be `textarea` instead of `input[type='text']`

--- src/components/InputText.tsx
+++ src/components/InputText.tsx
@@ -4,15 +4,23 @@ import styles from '../scss/InputText.module.scss';
 interface InputTextProps {
   placeholder: string;
   showError?: boolean;
+  value?: string;
+  onValueChange: (value: string) => void;
 }
 
-function InputText({ placeholder, showError }: InputTextProps) {
+function InputText({
+  placeholder,
+  showError,
+  value,
+  onValueChange,
+}: InputTextProps) {
   return (
     <div className={styles.inputTxt}>
-      <input
+      <textarea
         className={`${styles.text} ${showError && styles.error}`}
         placeholder={placeholder}
-        type="text"
+        value={value && value}
+        onChange={(e) => onValueChange(e.target.value)}
       />
       {showError && <p className={styles.errorMsg}>Can't be empty</p>}
     </div>
