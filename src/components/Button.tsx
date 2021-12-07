29053c9b5bb28a09a78d6612f01c7617c15c8724
Vinci Taylaran <37849496+vincitaylaran@users.noreply.github.com>
2021-12-06 18:40:49 -0700

    Give hover state and multiple background colors

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,13 +3,14 @@ import styles from '../scss/Button.module.scss';
 
 interface ButtonProps {
   children: React.ReactNode;
+  color?: 'purple' | 'blue' | 'red';
   onClick?: () => any;
 }
 
-function Button({ children, onClick }: ButtonProps) {
+function Button({ children, onClick, color }: ButtonProps) {
   return (
     <button
-      className={styles.button}
+      className={`${styles.button} ${color && styles[color]}`}
       onClick={() => {
         if (onClick) onClick();
       }}
