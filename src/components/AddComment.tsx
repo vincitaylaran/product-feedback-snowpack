dd3b57b0c03e82f8d63b647569d991bb4c23c5b2
Vinci Taylaran <37849496+vincitaylaran@users.noreply.github.com>
2021-12-07 13:09:05 -0700

    Complete AddComponent styles and functionality to limit character count

--- src/components/AddComment.tsx
+++ src/components/AddComment.tsx
@@ -1,18 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from './Card';
 import styles from '../scss/AddComment.module.scss';
 import Button from './Button';
 import InputText from './InputText';
 
 function AddComment() {
+  const maxCharCount: number = 250;
+  const [charCount, setCharCount] = useState<number>(maxCharCount);
+  const [text, setText] = useState<string>('');
+
+  const handleTextChange = (value: string): void => {
+    let currentCharCount: number = maxCharCount - value.length;
+
+    if (currentCharCount >= 0) {
+      setText(value);
+      setCharCount(currentCharCount);
+    }
+  };
+
+  const postComment = (): void => {
+    console.log(text);
+  };
+
   return (
     <Card className={styles.addComment}>
-      <h3 className={styles.heading}>Add Comment</h3>
-      <InputText placeholder="Type your comment here" />
-      {/* <div className={styles.footer}>
-        <div>Character count</div>
-        <Button>Post Comment</Button>
-      </div> */}
+      <h3 className={styles.addComment__heading}>Add Comment</h3>
+      <InputText
+        placeholder="Type your comment here"
+        value={text}
+        onValueChange={handleTextChange}
+      />
+      <div className={styles.addComment__footer}>
+        <p className={styles.charCount}>{charCount} Characters left</p>
+        <Button onClick={postComment}>Post Comment</Button>
+      </div>
     </Card>
   );
 }
