9217cfe1aecf208dc506985e82fb9bd7bb8e7666
Vinci Taylaran <37849496+vincitaylaran@users.noreply.github.com>
2021-12-14 16:58:51 -0700

    Update card to include icon

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,9 +5,15 @@ import styles from '../scss/Card.module.scss';
 interface CardProps {
   children: React.ReactNode;
   className?: string;
+  icon?: string;
 }
-function Card({ children, className }: CardProps) {
-  return <div className={`${styles.card} ${className}`}>{children}</div>;
+function Card({ children, className, icon }: CardProps) {
+  return (
+    <div className={`${styles.card} ${className}`}>
+      {icon && <img src={icon} alt="icon" className={styles.icon} />}
+      {children}
+    </div>
+  );
 }
 
 export default Card;
