a96a1e14d2e6d6cbed97fb3a45839da3451e218c
Vinci Taylaran <37849496+vincitaylaran@users.noreply.github.com>
2021-12-14 10:55:26 -0700

    Toggle textarea on Reply button click

--- src/components/CommentsCard.tsx
+++ src/components/CommentsCard.tsx
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from './Card';
 import type { Comment } from '../interfaces/productRequest.interface';
 import type { User } from '../interfaces/user.interface';
 import styles from '../scss/CommentsCard.module.scss';
 import { v4 as uuidv4 } from 'uuid';
+import Button from './Button';
 
 interface CommentsCardProps {
   comments: Comment[] | undefined;
 }
 
 function CommentsCard({ comments }: CommentsCardProps) {
+  const [toggledReplyId, setToggledReplyId] = useState<number | string>();
+
   // Temporary solution to fetching user's profile pic.
   const getUserImg = (user: User) => {
     const [firstName] = user.name.split(' ');
@@ -31,6 +34,14 @@ function CommentsCard({ comments }: CommentsCardProps) {
     }
   };
 
+  const toggleReplyTxtArea = (commentId: number | string): void => {
+    if (!toggledReplyId) {
+      setToggledReplyId(commentId);
+    } else {
+      setToggledReplyId(undefined);
+    }
+  };
+
   // TODO: create reply thread line for tablet
   return (
     <>
@@ -40,8 +51,8 @@ function CommentsCard({ comments }: CommentsCardProps) {
             {getCommentCount()} Comments
           </h3>
           <div className={styles.commentsCard__comments}>
-            {comments.map((comment) => (
-              <div className={styles.comment} key={uuidv4()}>
+            {comments.map((comment: Comment) => (
+              <div className={styles.comment} key={comment.id}>
                 <div className={styles.main}>
                   <div className={styles.heading}>
                     <img
@@ -55,45 +66,64 @@ function CommentsCard({ comments }: CommentsCardProps) {
                         @{comment.user.username}
                       </div>
                     </div>
-                    <button className={styles.replyBtn}>Reply</button>
+                    <button
+                      className={styles.replyBtn}
+                      onClick={() => toggleReplyTxtArea(comment.id)}
+                    >
+                      Reply
+                    </button>
                   </div>
                   <p className={styles.content}>{comment.content}</p>
+                  <ReplyArea isVisible={toggledReplyId === comment.id} />
                 </div>
 
                 {comment.replies && (
                   <div className={styles.replies}>
-                    {comment.replies.map((reply) => (
-                      <div
-                        className={`${styles.comment} ${styles.reply}`}
-                        key={uuidv4()}
-                      >
-                        <div className={styles.main}>
-                          <div className={styles.heading}>
-                            <img
-                              className={styles.profilePic}
-                              src={getUserImg(reply.user)}
-                              alt="Profile picture"
-                            />
-                            <div className={styles.userInfo}>
-                              <div className={styles.name}>
-                                {reply.user.name}
+                    {comment.replies.map((reply) => {
+                      if (!reply.id) reply.id = uuidv4();
+
+                      return (
+                        <div
+                          className={`${styles.comment} ${styles.reply}`}
+                          key={uuidv4()}
+                        >
+                          <div className={styles.main}>
+                            <div className={styles.heading}>
+                              <img
+                                className={styles.profilePic}
+                                src={getUserImg(reply.user)}
+                                alt="Profile picture"
+                              />
+                              <div className={styles.userInfo}>
+                                <div className={styles.name}>
+                                  {reply.user.name}
+                                </div>
+                                <div className={styles.username}>
+                                  @{reply.user.username}
+                                </div>
                               </div>
-                              <div className={styles.username}>
-                                @{reply.user.username}
-                              </div>
+                              {/* TODO: show textarea on toggling this button. */}
+                              <button
+                                className={styles.replyBtn}
+                                onClick={() => toggleReplyTxtArea(reply.id)}
+                              >
+                                Reply
+                              </button>
                             </div>
-                            {/* TODO: show textarea on toggling this button. */}
-                            <button className={styles.replyBtn}>Reply</button>
+                            <p className={styles.content}>
+                              <span className={styles.replyingTo}>
+                                @{reply.replyingTo}{' '}
+                              </span>
+                              <span>{reply.content}</span>
+                            </p>
+
+                            <ReplyArea
+                              isVisible={toggledReplyId === reply.id}
+                            />
                           </div>
-                          <p className={styles.content}>
-                            <span className={styles.replyingTo}>
-                              @{reply.replyingTo}{' '}
-                            </span>
-                            <span>{reply.content}</span>
-                          </p>
                         </div>
-                      </div>
-                    ))}
+                      );
+                    })}
                   </div>
                 )}
               </div>
@@ -104,6 +134,19 @@ function CommentsCard({ comments }: CommentsCardProps) {
         <h1>This request does not have comments</h1>
       )}
     </>
+  );
+}
+
+interface ReplyAreaProps {
+  isVisible?: boolean;
+}
+
+function ReplyArea({ isVisible = false }: ReplyAreaProps) {
+  return (
+    <div className={`${isVisible ? styles.replyTxtArea : styles.hidden}`}>
+      <textarea title="reply" />
+      <Button>Post Reply</Button>
+    </div>
   );
 }
 
